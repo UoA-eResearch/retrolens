0f12e18c089ee0e8454f0be00440d50251109f7a
Nick Young <nick.young@auckland.ac.nz>
2021-06-04 11:50:46 +1200

    add script to generate mapserver conf based on tifs in directory

---
+++ generate_mapserver_conf.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+from glob import glob
+from os.path import basename, splitext
+files = glob("/mnt/coastal/*/*.tif")
+
+print("""MAP
+    PROJECTION
+        "init=epsg:3857" # Output projection
+    END""")
+
+for filepath in files:
+    name = splitext(basename(filepath))[0]
+    print(f"""
+    LAYER
+        NAME "{name}"
+        DATA "{filepath}"
+        TYPE RASTER
+        PROJECTION
+            "init=epsg:2193"
+        END
+        STATUS ON
+        OFFSITE 255 255 255
+    END""")
+print("""
+    WEB
+        METADATA
+            "wms_title" "NZ Coastal Mosaic Server"
+            "wms_enable_request"  "*"
+        END
+    END
+END
+""")
