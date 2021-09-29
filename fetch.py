055529d84e845e25c824f13a4b4b622001aac680
Nick Young <nick.young@auckland.ac.nz>
2021-09-29 13:36:55 +1300

    modify fetch.py to process all CSVs in folder

--- fetch.py
+++ fetch.py
@@ -1,22 +1,25 @@
 #!/usr/bin/env python3
 
-import pandas as pd
-import requests
+import pandas as pd # Read CSV
+import requests # Make web requests
 import os
 from tqdm.auto import tqdm # Progress bar
+import glob # Matching filenames
 
-df = pd.read_csv("TZ_trial.csv")
+files = glob.glob("*.csv")
 
-for i,row in tqdm(df.iterrows(), total=df.shape[0]):
-    url = f"https://files.interpret.co.nz/Retrolens/Imagery/SN{row.Survey}/{row.Released_F}/High.jpg"
-    folder = f"{row.Region_1}/{row.Site}/{row.Date[-4:]}"
-    os.makedirs(folder, exist_ok=True)
-    filename = f"{folder}/{row.Released_F}.jpg"
-    if os.path.exists(filename):
-        print(f"{filename} exists, skipping")
-        continue
-    print(f"Fetching {url}")
-    r = requests.get(url)
-    print(f"Writing to {filename}")
-    with open(filename, "wb") as f:
-        f.write(r.content)
+for file in files:
+    df = pd.read_csv(file)
+    for i,row in tqdm(df.iterrows(), total=df.shape[0]):
+        url = f"https://files.interpret.co.nz/Retrolens/Imagery/SN{row.Survey}/{row.Released_F}/High.jpg"
+        folder = f"{row.Region_1}/{row.Site}/{row.Date[-4:]}"
+        os.makedirs(folder, exist_ok=True)
+        filename = f"{folder}/{row.Released_F}.jpg"
+        if os.path.exists(filename):
+            print(f"{filename} exists, skipping")
+            continue
+        print(f"Fetching {url}")
+        r = requests.get(url)
+        print(f"Writing to {filename}")
+        with open(filename, "wb") as f:
+            f.write(r.content)
