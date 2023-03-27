a5f4edb75106864087cf89cc4632f338e593339d
Nick Young <nick.young@auckland.ac.nz>
2023-03-27 14:40:59 +1300

    include MaxarImagery shoreline shapefiles

--- aggregate_shapefiles_to_geojson.py
+++ aggregate_shapefiles_to_geojson.py
@@ -9,14 +9,21 @@ import re
 
 def check_filename(filename):
     filename = os.path.splitext(os.path.basename(filename))[0]
-    return re.match(r'\d\w+\d{4}', filename)
+    return re.search(r'\d\w+\d{4}', filename)
 
 files = []
 for file in glob.iglob("/mnt/research/ressci201900060-RNC2-Coastal/Retrolens/**/Shorelines/*.shp", recursive=True):
     print(file)
     if check_filename(file):
         df = gpd.read_file(file)
-        df["filename"] = file.replace("/mnt/research/ressci201900060-RNC2-Coastal/Retrolens/", "")
+        df["filename"] = file.replace("/mnt/research/ressci201900060-RNC2-Coastal/", "")
+        files.append(df)
+
+for file in glob.iglob("/mnt/research/ressci201900060-RNC2-Coastal/MaxarImagery/HighFreq/**/Shorelines/*.shp", recursive=True):
+    print(file)
+    if check_filename(file):
+        df = gpd.read_file(file)
+        df["filename"] = file.replace("/mnt/research/ressci201900060-RNC2-Coastal/", "")
         files.append(df)
 
 df = gpd.GeoDataFrame(pd.concat(files)).to_crs(epsg=4326)
