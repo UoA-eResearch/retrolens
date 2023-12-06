77acc02465c58859025aab7ee941d018485b9402
Nick Young <nick.young@auckland.ac.nz>
2023-12-07 10:13:05 +1300

    rework aggregate_shapefiles_to_geojson.py. much faster now.

--- aggregate_shapefiles_to_geojson.py
+++ aggregate_shapefiles_to_geojson.py
@@ -6,25 +6,22 @@ import glob
 from tqdm import tqdm
 import os
 import re
+from tqdm.contrib.concurrent import thread_map, process_map
 
 def check_filename(filename):
-    filename = os.path.splitext(os.path.basename(filename))[0]
-    return re.search(r'\d\w+\d{4}', filename)
+    # This regex only matches shapefiles that contain something date-like in their names
+    match = re.search(r'/Shorelines/\w+\d\w+\d{4}\w*.shp$', filename)
+    return bool(match)
 
-files = []
-for file in glob.iglob("/mnt/research/ressci201900060-RNC2-Coastal/Retrolens/**/Shorelines/*.shp", recursive=True):
-    print(file)
-    if check_filename(file):
-        df = gpd.read_file(file)
-        df["filename"] = file.replace("/mnt/research/ressci201900060-RNC2-Coastal/", "")
-        files.append(df)
+# A complete filelist of the MaxarImagery & Retrolens folders, generated with the unix command:
+# find MaxarImagery/ Retrolens/ > ressci201900060-RNC2-Coastal_MaxarImagery_Retrolens_filelist.txt
+filelist = pd.read_csv("ressci201900060-RNC2-Coastal/Nick/ressci201900060-RNC2-Coastal_MaxarImagery_Retrolens_filelist.txt", header=None).iloc[:,0]
+df = filelist[filelist.apply(check_filename)].to_frame(name="filename")
 
-for file in glob.iglob("/mnt/research/ressci201900060-RNC2-Coastal/MaxarImagery/HighFreq/**/Shorelines/*.shp", recursive=True):
-    print(file)
-    if check_filename(file):
-        df = gpd.read_file(file)
-        df["filename"] = file.replace("/mnt/research/ressci201900060-RNC2-Coastal/", "")
-        files.append(df)
+def read(filename):
+    df = gpd.read_file(filename)
+    df["filename"] = filename
+    return df
 
-df = gpd.GeoDataFrame(pd.concat(files)).to_crs(epsg=4326)
+df = gpd.GeoDataFrame(pd.concat(process_map(read, df.filename))).to_crs(epsg=4326)
 df.to_file("shorelines.geojson")
