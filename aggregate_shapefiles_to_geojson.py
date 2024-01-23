130ea1301093c3f18a89e771114467137ccba709
Nick Young <nick.young@auckland.ac.nz>
2024-01-24 09:49:32 +1300

    check it all works on Windows with the drive mounted at Z:

--- aggregate_shapefiles_to_geojson.py
+++ aggregate_shapefiles_to_geojson.py
@@ -13,15 +13,21 @@ def check_filename(filename):
     match = re.search(r'/Shorelines/\w+\d\w+\d{4}\w*.shp$', filename)
     return bool(match)
 
+import platform
+if platform.system() == "Windows":
+  prefix = "Z:/"
+else:
+  prefix = "ressci201900060-RNC2-Coastal/"
+
 # A complete filelist of the MaxarImagery & Retrolens folders, generated with the unix command:
 # find MaxarImagery/ Retrolens/ > ressci201900060-RNC2-Coastal_MaxarImagery_Retrolens_filelist.txt
-filelist = pd.read_csv("ressci201900060-RNC2-Coastal/Nick/ressci201900060-RNC2-Coastal_MaxarImagery_Retrolens_filelist.txt", header=None).iloc[:,0]
+filelist = pd.read_csv(prefix + "/Nick/ressci201900060-RNC2-Coastal_MaxarImagery_Retrolens_filelist.txt", header=None).iloc[:,0]
 df = filelist[filelist.apply(check_filename)].to_frame(name="filename")
 
 def read(filename):
-    df = gpd.read_file(filename)
+    df = gpd.read_file(prefix + filename)
     df["filename"] = filename
     return df
 
-df = gpd.GeoDataFrame(pd.concat(process_map(read, df.filename))).to_crs(epsg=4326)
+df = gpd.GeoDataFrame(pd.concat(thread_map(read, df.filename))).to_crs(epsg=4326)
 df.to_file("shorelines.geojson")
